d23c9057614c7fc3aea38813c0e197568a2c8aae
Artem_Safon <coruscardea@gmail.com>
2026-07-10 19:40:21 +0300

    download.php

--- download.php
+++ download.php
@@ -2,4 +2,4 @@
 declare(strict_types=1);
 header('Content-Type: text/plain');
 header('Content-Disposition: attachment; filename="test.txt"', true,202);
-echo http_response_code();
+echo $_GET['text'];
