8530655e15442052c810fbaa3d883a98787b52a3
Artem_Safon <coruscardea@gmail.com>
2026-07-10 19:39:20 +0300

    counter.php

--- counter.php
+++ counter.php
@@ -1,5 +1,8 @@
 <?php
 
 session_start();
-
+if (isset($_SESSION['count'])=== true){
 echo 'Количество открытий страницы: ' . ($_SESSION['count']).PHP_EOL;
+}
+else
+   header('Location: sessionstart.php');
